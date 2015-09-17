30bcbd86782df28e779d80c700dda0477bf785a2
Roman Buldygin <froooks@gmail.com>
2015-09-17 11:21:54 +0600

    Receiver interface

---
+++ InProcTest/Domain/IReceiver.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Domain
+{
+    /// <summary>
+    /// Интрерфейс приёмщика сообщений
+    /// </summary>
+    interface IReceiver
+    {
+        event EventHandler<EventArgs> ResultReceived;
+    }
+}
