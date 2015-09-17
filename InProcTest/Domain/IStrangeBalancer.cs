7d83668b476d686310e6d8d88812c8ffb0f08b96
Roman Buldygin <froooks@gmail.com>
2015-09-17 10:38:43 +0600

    IStrangeBalancer interface

---
+++ InProcTest/Domain/IStrangeBalancer.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace Domain
+{
+    public interface IStrangeBalancer<T>
+    {
+        void Process(IEnumerable<Frame<T>> frames);
+
+        event EventHandler<ProcessedEventArgs> FramesProcessed;
+    }
+}
---
+++ InProcTest/Domain/ProcessedEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Domain
+{
+    /// <summary>
+    /// Событие завершения обработки части очереди
+    /// </summary>
+    public class ProcessedEventArgs : EventArgs
+    {
+        public int QueueId { get; }
+
+        public ProcessedEventArgs(int id)
+        {
+            QueueId = id;
+        }
+    }
+}
