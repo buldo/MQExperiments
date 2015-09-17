b79a660347121c0b0bbc1a4924cac2701fcdcaac
Roman Buldygin <froooks@gmail.com>
2015-09-17 12:50:34 +0600

    Save

---
+++ InProcTest/Domain/BaseWorker.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain
+{
+    public abstract class BaseWorker : IWorker
+    {
+        protected readonly IStatisticsCollector StatisticsCollector;
+        public int Id { get; }
+
+        protected BaseWorker(int id, IStatisticsCollector statisticsCollector)
+        {
+            Id = id;
+            StatisticsCollector = statisticsCollector;
+        }
+
+        public abstract void StartProcessing();
+
+    }
+}
---
+++ InProcTest/NanoMsgRealization/NanoWorker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace NanoMsgRealization
+{
+    public class NanoWorker : BaseWorker
+    {
+        public NanoWorker(int id, IStatisticsCollector statisticsCollector) : base(id, statisticsCollector)
+        {
+        }
+
+        public override void StartProcessing()
+        {
+
+        }
+    }
+}
